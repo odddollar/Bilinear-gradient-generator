40932b83a78d6a74b28e5a464de18e85d25722f5
Simon Eason <sie.eason@gmail.com>
2021-01-15 16:00:13 +0800

    fixed imports for go.mod

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module Bilinear-gradient-generator
 
 go 1.15
 
-require github.com/schollz/progressbar v1.0.0
+require github.com/schollz/progressbar/v3 v3.7.3
--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/schollz/progressbar"
+	"github.com/schollz/progressbar/v3"
 	"image"
 	"image/color"
 	"image/png"
